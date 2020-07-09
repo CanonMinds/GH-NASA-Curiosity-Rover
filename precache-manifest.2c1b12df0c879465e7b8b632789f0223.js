d993e604a315f6588ffc76173c51c47cbd2cfb5d
Ray <balafranzraymond@gmail.com>
2020-07-09 12:49:12 +0800

    Updates

---
+++ precache-manifest.2c1b12df0c879465e7b8b632789f0223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437e8c094d8f905711a3580266fc8c53",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "664cc11274cf686a4eec",
+    "url": "/gh-pages-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd34df4b7c8da86809e0",
+    "url": "/gh-pages-app/static/js/2.9f75f065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-app/static/js/2.9f75f065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664cc11274cf686a4eec",
+    "url": "/gh-pages-app/static/js/main.f6f2da8d.chunk.js"
+  },
+  {
+    "revision": "0d0f2f53be3fa93aa88b",
+    "url": "/gh-pages-app/static/js/runtime-main.ea4dee6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
